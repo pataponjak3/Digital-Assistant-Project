2f5322e673edbc1f2aea9156245e962102042b20
pataponjak3 <dinis203@gmail.com>
2025-05-08 15:01:48 +0100

    Changed interface

--- src/core/main.py
+++ src/core/main.py
@@ -1,14 +1,18 @@
+#from ..ui.gui import AssistantGUI
+from .config import ModuleLoader
 import sys
-import os
-"""from dotenv import load_dotenv
-
-load_dotenv()  # Load .env file
-print("Loading environment variables...")"""
-
-from ..ui.gui import AssistantGUI
+from ..ui.assistant_interface import AssistantGUI
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 def main():
-    print("Launching Digital Assistant GUI...")
-    AssistantGUI()
+    #print("Launching Digital Assistant GUI...")
+    #AssistantGUI()
+    module_loader = ModuleLoader()
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = AssistantGUI(module_loader)
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__": main()
